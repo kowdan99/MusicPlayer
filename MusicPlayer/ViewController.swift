867714c156f8b62cce774d22df74c6f0466cce81
Youssof Kowdan <v-yokowd@microsoft.com>
2019-07-06 13:13:50 -0700

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Youssof Kowdan on 7/6/19.
+//  Copyright © 2019 Youssof Kowdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
